3e6b71e3af946deec3eddd884f2876be66e11376
tungx1 <tungphamthanhcdt@gmail.com>
2023-10-31 17:03:12 +0700

    sss

--- ServerNodeJs/src/Server.mjs
+++ ServerNodeJs/src/Server.mjs
@@ -8,21 +8,18 @@ const io = new Server(server);
 let userConnects = [];
 io.on('connection', (socket) => {
   socket.on('login', (user) => {
-    const existUser = userConnects.find((item) => item.id === user.id);
+    const existUser = userConnects.find((item) => item.user.id === user.id);
     if (!existUser) {
-      userConnects.push(user);
-      console.log(user);
-      socket.emit('userConnects', userConnects);
+      userConnects.push({ user: user, id: socket.id });
+      // console.log('login', userConnects);
+      io.emit('userConnects', userConnects);
     }
   });
   socket.on('logout', (user1) => {
-    console.log(user1);
-    userConnects = userConnects.filter((item) => item.id !== user1.id);
-    socket.emit('userConnects', userConnects);
+    userConnects = userConnects.filter((item) => item.user.id !== user1.id);
+    // console.log('logout', userConnects);
+    io.emit('userConnects', userConnects);
   });
-  let ids = [];
-  ids = userConnects.map((item, i) => item.id);
-  console.log(ids);
 });
 
 export { app, server };
